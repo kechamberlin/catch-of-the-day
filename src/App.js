1c3fc75c1f2d051495666e75eba83581898258f2
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-03 19:30:29 -0700

    Imported StorePicker and put it in return statement.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import StorePicker from './components/StorePicker';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        
+        <StorePicker />
       </header>
     </div>
   );
